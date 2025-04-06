be4545426d2e43a21bb5a05da7f0f186fd102b39
Hasibul Hasan <hasibulhasanx0902@gmail.com>
2025-04-06 13:04:19 +0600

    project done

--- scripts/alphaclash.js
+++ scripts/alphaclash.js
@@ -3,18 +3,77 @@
 //     document.getElementById('playScreen').classList.remove('hidden')
 
 // }
+// keyBoard Function
+document.addEventListener("keyup", keyPress);
 
+function keyPress(element) {
+  let scoreCount = 0;
+  const playerPress = element.key;
+ if(playerPress==='Escape'){
+    finalScore()
+ }
+
+  const expectedPressUppercase =
+    document.getElementById("displayText").innerText;
+  const expectedPress = expectedPressUppercase.toLocaleLowerCase();
+
+  if (playerPress === expectedPress) {
+    continuePlay();
+    removeBackgroundColorById(playerPress);
+
+    const score = getIntValueById("scoreBoard");
+    const newScore = score + 1;
+    setIntValueById("scoreBoard", newScore);
+    setIntValueById("final-score", newScore);
+  } else {
+    const life = getIntValueById("lifeBoard");
+    const newLife = life - 1;
+    setIntValueById("lifeBoard", newLife);
+
+    if (newLife <= 0) {
+      finalScore();
+    }
+  }
+}
+
+// continue playSection
 function continuePlay() {
-   const randomAlphabet= getRandomAlphabet()
-   
-   document.getElementById('displayText').innerText=randomAlphabet
-   console.log(randomAlphabet)
+  const randomAlphabet = getRandomAlphabet();
 
-   setBackgroundColorById(randomAlphabet)
+  document.getElementById("displayText").innerText = randomAlphabet;
+
+  setBackgroundColorById(randomAlphabet);
 }
 
-function homeScreen(){
-    hideElementById('homeScreen')
-    showElementById('playScreen')
-    continuePlay()
+function homeScreen() {
+  hideElementById("homeScreen");
+  showElementById("playScreen");
+  continuePlay();
 }
+
+function finalScore() {
+  hideElementById("playScreen");
+  showElementById("finalScoreBoard");
+}
+
+function tryAgain() {
+  showElementById("playScreen");
+  hideElementById("finalScoreBoard");
+  const removeColTxt= getInnerText('displayText')
+  
+  removeBackgroundColorById(removeColTxt)
+
+  continuePlay();
+  setIntValueById('scoreBoard',0)
+  setIntValueById('lifeBoard',5)
+
+  
+  
+
+}
+
+// function tryAgain(){
+//     hideElementById('finalScoreBoard')
+//     showElementById('playScreen')
+
+// }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,28 +1,58 @@
-function hideElementById(elementId){
-    document.getElementById(elementId).classList.add('hidden')
+// hide and show function
+
+function hideElementById(elementId) {
+  document.getElementById(elementId).classList.add("hidden");
 }
 
-function showElementById(elementId){
-    document.getElementById(elementId).classList.remove('hidden')
+function showElementById(elementId) {
+  document.getElementById(elementId).classList.remove("hidden");
 }
 
-function getRandomAlphabet(){
-    const string='abcdefghijklmnopqrstuvwxyz'
-    const alphabets=string.split('')
-    
-    const randomNum=Math.random()*25
-    const number=Math.round(randomNum)
-    
-    const randomAlphabet=alphabets[number]
-    return randomAlphabet;
+// get any random alphabet function
+
+function getRandomAlphabet() {
+  const string = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = string.split("");
+
+  const randomNum = Math.random() * 25;
+  const number = Math.round(randomNum);
+
+  const randomAlphabet = alphabets[number];
+  return randomAlphabet;
 }
 
-function setBackgroundColorById(keyElement){
+// keyboard color set and remove
 
-    const element=document.getElementById(keyElement)
-    element.classList.add('text-orange-800')
-    element.classList.add('text-2xl')
-    
-    
+function setBackgroundColorById(keyElement) {
+  const element = document.getElementById(keyElement);
+  element.classList.add("text-orange-800");
+  element.classList.add("text-2xl");
+}
 
+function removeBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("text-orange-800");
+  element.classList.remove("text-2xl");
+}
+
+// common int value find and set function
+
+function getIntValueById(getId) {
+  const getElement = document.getElementById(getId);
+  const getText = getElement.innerText;
+  const getValue = parseInt(getText);
+  return getValue;
+}
+
+function setIntValueById(getId, value) {
+  const setElement = document.getElementById(getId);
+  setElement.innerText = value;
+}
+
+// find any element inner text
+
+function getInnerText(elementId){
+   const element=document.getElementById(elementId)
+   const innerText=element.innerText.toLocaleLowerCase()
+   return innerText
 }
